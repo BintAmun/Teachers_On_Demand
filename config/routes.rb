e26d79d42385824f2674a9dd7349f555dba73d93
Daniel Enriquez <daniel.enm17@gmail.com>
2022-10-22 15:06:19 -0500

    Test comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # Hello pana
   # Defines the root path route ("/")
   # root "articles#index"
 end
