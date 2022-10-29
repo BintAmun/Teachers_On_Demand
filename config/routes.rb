ecae85e6336c5cdd61d42194d144f1b2859b4f11
fernandozuniga <92114318+asterixf@users.noreply.github.com>
2022-10-29 17:17:27 -0500

    erase symbol
    erased symbol from conflict resolution that was causing issues

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   #get "lessons/new", to: "lessons#new"
   #post "lessons/create", to: "lessons#create"
   #get "lessons/:id", to: "lessons#show"
-=======
   devise_for :users
   # root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
