afda3e0b8f01a6249df8397645ad948b936071b9
TaishoIwamoto <h.m.tai.26@gmail.com>
2022-10-29 15:46:36 -0500

    fixed lessons edit, update, and delete

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -18,6 +18,22 @@ class LessonsController < ApplicationController
     redirect_to lesson_path(@lesson.id)
   end
 
+  def edit
+    @lesson = Lesson.find_by(id: params[:id])
+  end
+
+  def update
+    @lesson = Lesson.find_by(id: params[:id])
+    @lesson.update(lesson_params)
+    redirect_to @lesson
+  end
+
+  def destroy
+    @lesson = Lesson.find_by(id: params[:id])
+    @lesson.destroy
+    redirect_to lessons_path, status: :see_other
+  end
+
   private
 
   def lesson_params
