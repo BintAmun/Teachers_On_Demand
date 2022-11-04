b7d32ec3b03c12b981b6c32fc342bd89b06b767a
fernando zuniga <carlosfernando.zh@gmail.com>
2022-11-04 17:35:28 -0600

    added search bar to lessons

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,12 @@
 class Lesson < ApplicationRecord
   has_many :schedules
   has_one_attached :photo
+
+include PgSearch::Model
+
+  pg_search_scope :search_bar_lessons,
+  against: [ :name, :description ],
+  using: {
+    tsearch: { prefix: true}
+  }
 end
